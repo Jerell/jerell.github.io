36bd323b98179b8df3c5843a620f62c8f5e6fc7f
Jerell <jerell1998@gmail.com>
2016-12-31 14:03:20 +0000

    Read and Find Dupes
    Finished the functions to read areas of the sudoku grid. Created
    functions to find duplicate cell values in relevant sections.
    
    Added a link for my research source.
    Added Prism.js to colour code snippets.
    
    Next steps:
    Create a system for inputting data through the webpage.
    Create an automated loop that reads a cell and does all the relevant
    checks

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,115 +1,173 @@
-$(document).ready(function() {
-  //--The size of the grid will be decided by user input. For initial testing this will be 4x4 but once it works it should default to 9x9
-  var maxVal = 4;
-  var defaultVal = 0; //  A cell's default value should be zero
 
-  //--Rows and columns won't actually exist but be read by functions
-  //--checkRow(1) will read the [1]st [n] cells in the first [n] blocks in a gris of size [n^2]
-  //--checkRow(2) will read the next [n] cells in the first [n] blocks
-  //--Assuming grid size 4:
-  //--checkRow(3) will read the first [n] cells in the second [n] blocks
+//--The size of the grid will be decided by user input. For initial testing this will be 4x4 but once it works it should default to 9x9
+var maxVal = 4;
+var defaultVal = 0; //  A cell's default value should be zero
 
-  //--[max] is the side length of a block squared
-  //--I will need to use the side length a lot so:
-  var side = Math.sqrt(maxVal);
+var side = Math.sqrt(maxVal);
 
-  //--checkRow(num) reads [side] cells from the [Math.ceil(num/side)] row of blocks
+//  cell < block < Grid
+
+//  cells have coordinates, values, and possibilities - max is the size of the grid which is one block side length squared
 
 
-  //  cell < block < Grid
-
-  //  cells have coordinates, values, and possibilities - max is the size of the grid which is one block side length squared
-
-
-  //Cell functions
-  function setAllPossible(max){       //  This method sets all numbers up to
-    for(var i=0; i<max; i++){       //  the specified maximum
-      this.possibilities[i] = true; //  to be possibilities
-    }                               //  for this cell
-  }                                 //
-  function removePos(p){
-    this.possibilities[p-1] = false;
-  }
-  function addPos(p){
-    this.possibilities[p-1] = true;
-  }
-  function setValue(newValue){     //  Set the new value and remove other possibilities
-    this.value = newValue;
-    for(var i=0; i<this.max; i++){
-      if(this.possibilities[i] != newValue){
-        removePos(i);
-      }
+//Cell functions
+function setAllPossible(max){       //  This method sets all numbers up to
+  for(var i=0; i<max; i++){       //  the specified maximum
+    this.possibilities[i] = true; //  to be possibilities
+  }                               //  for this cell
+}                                 //
+function removePos(p){
+  this.possibilities[p-1] = false;
+}
+function addPos(p){
+  this.possibilities[p-1] = true;
+}
+function setValue(newValue){     //  Set the new value and remove other possibilities
+  this.value = newValue;
+  for(var i=0; i<this.max; i++){
+    if(this.possibilities[i] != newValue){
+      removePos(i);
     }
   }
-  //Cell Object
-  function Cell(xcoord, ycoord, val, max){
-    //--Cell info
-    this.x = xcoord;
-    this.y = ycoord;
-    this.value = val;
-    this.max = max;
+}
+//Cell Object
+function Cell(xcoord, ycoord, val, max){
+  //--Cell info
+  this.x = xcoord;
+  this.y = ycoord;
+  this.value = val;
+  this.max = max;
+  //--Possibilities
+  this.possibilities = [];          //  (Empty array of possibilities)
+  this.setAllPossible = setAllPossible;
+  this.setAllPossible(this.max);       // The method is called when the cell is created
+  this.removePos = removePos;
+  this.addPos = addPos;
+  this.setValue = setValue;
+}
 
-    //--Possibilities
-    this.possibilities = [];          //  (Empty array of possibilities)
-    this.setAllPossible(this.max);       // The method is called when the cell is created
-    this.removePos = removePos;
-    this.addPos = addPos;
-    this.setValue = setValue;
+// Grid functions
+function populate(max){
+  for(var i=0; i< max * max; i++){  //  A grid will have [maxVal] squared cells
+    var x = i % max;                    //  A cell's x coord is the cell number mod [maxVal]
+    var y = Math.floor(i/max);          //  A cell's y coord is the cell number divided by [maxVal] rounded down
+    this.cells[i] = new Cell(x, y, defaultVal, max);  //  [defaultVal] should be zero
   }
+}
+function readCell(x, y) {
+  var cellNum = y * maxVal + x;
+  return this.cells[cellNum];
+}
+function readRow(rowNum){
+  var row = [];
+  var startCellNum = (rowNum - 1) * maxVal;
+  for(var i=0; i<maxVal; i++){
+    row[i] = this.cells[startCellNum + i];
+  }
+  return row;
+}
+function readColumn(colNum){
+  var col = [];
+  var startCellNum = colNum;
+  for(var i=0; i<maxVal; i++){
+    col[i] = this.cells[startCellNum + maxVal*i];
+  }
+  return col;
+}
+function readBlock(blockNum){
+  var block = [];
+  var startCellNum = ((blockNum-1) % side) * side + Math.floor((blockNum-1)/side)*maxVal*side;
+  for(var i = 0; i < maxVal; i++){
+    var cellNum = Math.floor(i/side)*maxVal + i % side;
+    block[i] = this.cells[cellNum];
+  }
+  return block;
 
-  // Grid functions
-  function populate(max){
-    for(var i=0; i< max * max; i++){  //  A grid will have [maxVal] squared cells
-      var x = i % max;                    //  A cell's x coord is the cell number mod [maxVal]
-      var y = Math.floor(i/max);          //  A cell's y coord is the cell number divided by [maxVal] rounded down
-      this.cells[i] = new Cell(x, y, defaultVal, max);  //  [defaultVal] should be zero
+}
+
+function findDupes(cellList){
+  var list = cellList;
+  if(typeof(list[0]) === 'object'){
+    for(var i = 0; i < list.length; i++){
+      list[i] = list[i].value;
     }
   }
-  function readCell(x, y) {
-    var cellNum = y * maxVal + x;
-    return this.cells[cellNum];
-  }
-  function readRow(rowNum){
-    var row = [];
-    var startCellNum = (rowNum - 1) * maxVal;
-    for(var i=0; i<maxVal; i++){
-      row[i] = this.cells[startCellNum + i];
-    }
-    return row;
-  }
-  function readColumn(colNum){
-    var col = [];
-    var startCellNum = colNum;
-    for(var i=0; i<maxVal; i++){
-      col[i] = this.cells[startCellNum + maxVal*i];
-    }
-    return col;
-  }
-  function checkRow(row){
-    var complete = true;
+  var complete = true;
+  var sorted = list;
+  sorted.sort();
+  var duplicates = [];
+
+  for(var j=0; j < sorted.length; j++){
     //  check there is a number in every cell
-    for(var i=0; i<maxVal; i++){
-      if(row[i] === 0){
-        complete = false;
+    if(list[j] === 0){
+      complete = false;
+    }
+    //  compare
+    if(sorted[j] == sorted[j+1]){
+      if(duplicates.includes(sorted[j]) == false){
+        duplicates.push(sorted[j]);
       }
     }
-    //  second loop to check each cell is unique after making sure the row
-
   }
-  //Grid Object
-  function Grid(sideLength){
-    this.cells = [];
-    this.populate = populate;
-    this.populate(sideLength);  //  Populate this grid with cells with a maximum value of [sideLength]
-
-    this.readCell = readCell;
-    this.readRow = readRow;
-    this.readColumn = readColumn;
+  return duplicates;
+}
+function isComplete(list){
+  var status = true;
+  for(var i=0; i<list.length; i++){
+    if(list[i] === 0){
+      status = false;
+    }
   }
+  return status;
+}
 
-  //--End
+function checkRow(rowNum){
+  var dupes = findDupes(this.readRow(rowNum));
+  var noDupesExist = dupes.length === 0 ? true : false;
+  var duplicate_string = "Row " + rowNum + " contains too many instances of " + dupes;
+  if(noDupesExist){
+    duplicate_string = "There are no duplicates in row " + rowNum;
+  }
+  return [duplicate_string, noDupesExist];
+}
+function checkColumn(colNum){
+  var dupes = findDupes(this.readColumn(colNum));
+  var noDupesExist = dupes.length === 0 ? true : false;
+  var duplicate_string ="Column " + colNum + " contains too many instances of " + dupes;
+  if(noDupesExist){
+    duplicate_string = "There are no duplicates in column " + colNum;
+  }
+  return [duplicate_string, noDupesExist];
+}
+function checkBlock(blockNum){
+  var dupes = findDupes(this.readRow(blockNum));
+  var noDupesExist = dupes.length === 0 ? true : false;
+  var duplicate_string = "Block " + blockNum + " contains too many instances of " + dupes;
+  if(noDupesExist){
+    duplicate_string = "There are no duplicates in blck " + blockNum;
+  }
+  return [duplicate_string, noDupesExist];
+}
 
-});
+//Grid Object
+function Grid(sideLength){  // Side options will be limited to square numbers
+  var self = this;
+  this.cells = [];
+  this.max   = sideLength;
+  this.populate = populate;
+  this.populate(this.max);
 
+  this.readCell    = readCell;
+  this.readRow     = readRow;
+  this.readColumn  = readColumn;
+  this.readBlock   = readBlock;
 
-//	add references to the research area. look for the guardian article.
+  this.findDupes   = findDupes;
+  this.isComplete  = isComplete;
+
+  this.checkRow    = checkRow;
+  this.checkColumn = checkColumn;
+  this.checkBlock  = checkBlock;
+}
+var g4 = new Grid(4);
+//--End
