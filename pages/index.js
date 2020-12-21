49e1005761f0ded9ac7d12f117c09f728d8bee5d
Jerell James <jerell1998@gmail.com>
2020-12-21 01:51:00 +0000

    next

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hey, I'm Jerell. I'm a <i>software developer/data analyst</i> who
+          primarily uses <strong>JavaScript</strong> and <strong>Python</strong>
+        </p>
+        <p>I'll have more to show here soon.</p>
+      </section>
+    </Layout>
+  );
+}
