875c16f502b42d705503dec68ce923ee0f25e3fa
Jerell James <jerell1998@gmail.com>
2020-12-21 12:10:02 +0000

    file blocked

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, home }) {
     <>
       <div className={styles.container}>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="./favicon.ico" />
           <meta name="description" content={`${name}'s portfolio of work`} />
           <meta
             property="og:image"
