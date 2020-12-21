74de965748d33a9a7be10b66fc45cba552ce59ec
Jerell James <jerell1998@gmail.com>
2020-12-21 02:07:57 +0000

    work

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,22 @@ export default function Home({ allPostsData }) {
         </p>
         <p>I'll have more to show here soon.</p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Work</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
