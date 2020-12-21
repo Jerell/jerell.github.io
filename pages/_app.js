5fd50def43457069d04e4b75b0cebc0a98d59efc
Jerell James <jerell1998@gmail.com>
2020-12-21 02:52:49 +0000

    footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,35 @@
-import "../styles/global.css";
+import "../styles/global.scss";
 import { parseISO, format } from "date-fns";
 import { name } from "../components/layout";
 
+const links = [
+  { text: "GitHub", url: "https://github.com/jerell" },
+  {
+    text: "LinkedIn",
+    url: "https://www.linkedin.com/in/jerell-james-831b12158/",
+  },
+];
+
+const FootLink = ({ text, url }) => {
+  return (
+    <>
+      <a href={url} target="_">
+        {text}
+      </a>{" "}
+    </>
+  );
+};
+
 export default function App({ Component, pageProps }) {
-  const date = new Date();
+  const date = new Date("2020-01-01");
 
   return (
     <>
       <Component {...pageProps} />
       <footer className="bg-gray-100 w-full text-center">
-        <a href="https://github.com/jerell" target="_">
-          GitHub
-        </a>
+        {links.map((l) => (
+          <FootLink {...l}></FootLink>
+        ))}
         <p>
           &#169;{" "}
           <time dateTime={date}>
