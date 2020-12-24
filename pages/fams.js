b040ce84388b4af6615bcf099799081a75af321e
Jerell James <jerell1998@gmail.com>
2020-12-24 22:28:36 +0000

    grid

--- pages/fams.js
+++ pages/fams.js
@@ -4,13 +4,34 @@ import utilStyles from "../styles/utils.module.css";
 
 const name = "FAMS";
 
-export default function Vis() {
+export default function Fams() {
   return (
     <Demo name={name}>
       <Head>
         <title>{name}</title>
       </Head>
-      <section className={utilStyles.headingMd}></section>
+      <section className={utilStyles.headingMd}>
+        <div className="grid grid-cols-3 gap-4 text-center rounded-t-xl overflow-hidden bg-gradient-to-r from-yellow-50 to-yellow-100 bg-white p-8">
+          <div className="col-span-2 row-span-2 bg-yellow-500 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+            1
+          </div>
+          <div className="bg-yellow-500 rounded-md h-12 flex items-center justify-center text-white text-2xl font-extrabold">
+            2
+          </div>
+          <div className="bg-yellow-500 rounded-md h-12 flex items-center justify-center text-white text-2xl font-extrabold">
+            3
+          </div>
+          <div className="bg-yellow-500 rounded-md h-12 flex items-center justify-center text-white text-2xl font-extrabold">
+            4
+          </div>
+          <div className="bg-yellow-500 rounded-md h-12 flex items-center justify-center text-white text-2xl font-extrabold">
+            5
+          </div>
+          <div className="bg-yellow-500 rounded-md h-12 flex items-center justify-center text-white text-2xl font-extrabold">
+            6
+          </div>
+        </div>
+      </section>
     </Demo>
   );
 }
