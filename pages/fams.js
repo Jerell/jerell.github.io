2ebc6cc733714c9636ba3269bcc9e5ef61523ed3
Jerell James <jerell1998@gmail.com>
2020-12-27 15:27:08 +0000

    alignment

--- pages/fams.js
+++ pages/fams.js
@@ -14,8 +14,8 @@ export default function Fams() {
       </Head>
       <section className={utilStyles.headingMd}>
         <div className="grid grid-cols-6 gap-4 text-center rounded-t-xl overflow-hidden bg-gradient-to-r from-green-50 to-green-100 bg-white p-8">
-          <div className="col-span-2 row-span-2 bg-gray-300 rounded-md flex flex-col items-center justify-center text-white text-2xl font-extrabold">
-            <p className="text-left pl-1">Live data stream</p>
+          <div className="col-span-2 row-span-2 bg-gray-300 rounded-md flex flex-col 2xl:items-center text-white text-2xl font-extrabold">
+            <p className="pl-1 text-center">Live data stream</p>
             <Horizon></Horizon>
           </div>
           <div className="col-span-2 row-span-2 bg-green-500 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
