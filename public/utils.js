983cdb7476f4e646f61a0731484efab44c667b3a
Jerell James <jerell1998@gmail.com>
2021-01-08 10:40:18 +0000

    formula changes

--- public/utils.js
+++ public/utils.js
@@ -18,19 +18,21 @@ export function pressureDrop({ length, diameter, flowrate }) {
   length = length * 1000; // km to m
   diameter = diameter * 0.0254; // inches to m
   const area = (diameter / 2) ** 2 * Math.PI;
-  const density = 1.976;
+  const density = 92.92;
   const f = 0.013615299; // Darcy friction factor
 
   const mtpaToM3ps = (mtpa) => {
-    const tpa = mtpa / 10 ** 6;
-    const kgpa = tpa / 1000;
+    const tpa = mtpa * 10 ** 6;
+    const kgpa = tpa * 1000;
     const m3pa = kgpa / density;
     const secondsInAYear = 60 * 60 * 24 * 365.25;
-    return m3pa / secondsInAYear;
+    const r = m3pa / secondsInAYear;
+    console.log(r);
+    return r;
   };
   const velocity = mtpaToM3ps(flowrate) / area;
-
-  const drop = ((4 * density * velocity ** 2 * f) / (2 * diameter)) * length;
+  console.log({ velocity });
+  const drop = ((density * velocity ** 2 * f) / (2 * diameter)) * length;
   const paToBar = (pa) => pa / 10 ** 5;
   return drop ? paToBar(drop) : "-";
 }
