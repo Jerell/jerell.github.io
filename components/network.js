e54168c7be38b26482072e41a3ed208163d028e7
Jerell James <jerell1998@gmail.com>
2021-02-12 16:10:39 +0000

    neater tick function

--- components/network.js
+++ components/network.js
@@ -210,18 +210,10 @@ export default function Network({ selectNode, setNodeName, handleNodeHover }) {
           });
 
         link
-          .attr("x1", function (d) {
-            return d.source.x;
-          })
-          .attr("y1", function (d) {
-            return d.source.y;
-          })
-          .attr("x2", function (d) {
-            return d.target.x;
-          })
-          .attr("y2", function (d) {
-            return d.target.y;
-          });
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
 
         node.attr("transform", (d) => `translate(${d.x}, ${d.y})`);
       }
