72b97e531001fb65efeae237bd219e07cbbe1b25
Jerell James <jerell1998@gmail.com>
2021-12-05 23:17:23 +0000

    build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,34 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+// const getLocalPackages = require('./scripts/getLocalPackages');
+
+// const localPackages = getLocalPackages.getLocalPackages();
+// const withTM = require('next-transpile-modules')();
+const nextConfig = {
+  webpack: (config, options) => {
+    return config;
+  },
+  eslint: {
+    // ESLint managed on the workspace level
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true,
+  },
+};
+
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig
+);
+
+module.exports = config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: [],
+  purge: ['./**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
