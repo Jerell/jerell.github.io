f4fd257c3356e57f33ab3f7e0ce2251ac5c14447
Jerell James <jerell1998@gmail.com>
2021-12-05 23:38:20 +0000

    build

--- next.config.js
+++ next.config.js
@@ -1,34 +1,9 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const isProd = process.env.NODE_ENV === 'production';
 
-// const getLocalPackages = require('./scripts/getLocalPackages');
-
-// const localPackages = getLocalPackages.getLocalPackages();
-// const withTM = require('next-transpile-modules')();
-const nextConfig = {
-  webpack: (config, options) => {
-    return config;
-  },
-  eslint: {
-    // ESLint managed on the workspace level
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    disableStaticImages: true,
-  },
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? 'https://cdn.statically.io/gh/jerell/jerell.github.io/gh-pages/'
+    : '',
+  // distDir: "build",
 };
-
-const config = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
-        optimizeImages: false,
-      },
-    ],
-  ],
-  nextConfig
-);
-
-module.exports = config;
