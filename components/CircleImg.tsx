8524da5682f850e32848fe7c51d62caff56aca77
Jerell James <jerell1998@gmail.com>
2021-12-05 23:44:50 +0000

    build

--- components/CircleImg.tsx
+++ components/CircleImg.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
-import me from '@/public/img/mecropblur.jpg';
-
 const CircleImg = () => {
   return (
     <div className='rounded-full h-36 w-36 overflow-hidden'>
-      <Image src={me} alt='Picture of Jerell' />
+      <img src={'/img/mecropblur.jpg'} alt='Picture of Jerell' />
     </div>
   );
 };
