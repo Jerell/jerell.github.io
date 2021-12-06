ec05a4c5997b8353c89343d388bbd4c89ed8558a
Jerell James <jerell1998@gmail.com>
2021-12-06 22:46:23 +0000

    pls

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? 'https://cdn.statically.io/gh/jerell/jerell.github.io/gh-pages/'
-    : '',
+  assetPrefix: '.',
   swcMinify: false, // it should be false by default
   // distDir: "build",
 };
