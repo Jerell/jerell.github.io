5142a2f5fff4ca37667e82821d841ccd431c837e
Jerell <jerell1998@gmail.com>
2021-12-06 22:33:03 +0000

    Deploying to gh-pages from  @ 393aa165b72f6eeb861ab375917a148ef355b10c 🚀

--- _next/static/-Ek_rQ4AKWqqC6ACJlv63/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f3197b8f1472e142.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
