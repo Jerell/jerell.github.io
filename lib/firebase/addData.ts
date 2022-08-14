5b0fc3dc2d23aac0ca00ab846f3eff957f271280
Jerell James <jerell1998@gmail.com>
2022-08-14 10:29:26 +0100

    auth

--- lib/firebase/addData.ts
+++ lib/firebase/addData.ts
@@ -1,10 +1,13 @@
 import { collection, addDoc, Firestore } from 'firebase/firestore';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
 export default async function addData(
   db: Firestore,
   collectionName: string,
   content: object
 ) {
+  const auth = getAuth();
+  await signInAnonymously(auth);
   try {
     const docRef = await addDoc(collection(db, collectionName), content);
     console.log('Document written with ID: ', docRef.id);
--- lib/firebase/readData.ts
+++ lib/firebase/readData.ts
@@ -5,12 +5,20 @@ import {
   DocumentData,
   query,
   where,
+  orderBy,
 } from 'firebase/firestore';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
 export default async function readData(db: Firestore, collectionName: string) {
-  const collectionRef = collection(db, collectionName);
+  const auth = getAuth();
+  await signInAnonymously(auth);
 
-  const q = query(collectionRef, where('approved', '==', true));
+  const collectionRef = collection(db, collectionName);
+  const q = query(
+    collectionRef,
+    where('approved', '==', true),
+    orderBy('date', 'desc')
+  );
 
   const querySnapshot = await getDocs(q);
   const data: DocumentData[] = [];
