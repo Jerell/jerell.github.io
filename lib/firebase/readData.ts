13806e34792759a540b8a255e821da3e32b24b4d
Jerell James <jerell1998@gmail.com>
2022-08-14 10:12:32 +0100

    only show approved

--- lib/firebase/readData.ts
+++ lib/firebase/readData.ts
@@ -3,10 +3,16 @@ import {
   getDocs,
   Firestore,
   DocumentData,
+  query,
+  where,
 } from 'firebase/firestore';
 
 export default async function readData(db: Firestore, collectionName: string) {
-  const querySnapshot = await getDocs(collection(db, collectionName));
+  const collectionRef = collection(db, collectionName);
+
+  const q = query(collectionRef, where('approved', '==', true));
+
+  const querySnapshot = await getDocs(q);
   const data: DocumentData[] = [];
   querySnapshot.forEach((doc) => {
     data.push(doc.data());
