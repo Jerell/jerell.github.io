c03e0f912acadd1fb1ff2e70220f91b7f0aae3a4
Jerell James <jerell1998@gmail.com>
2023-08-16 11:16:11 +0100

    layouts

---
+++ src/app/(other)/balloons/page.tsx
@@ -0,0 +1,9 @@
+export default function BalloonPage() {
+  return (
+    <div className='max-w-2xl w-full flex flex-col gap-4'>
+      <section>
+        <h2>Balloons</h2>
+      </section>
+    </div>
+  );
+}
