8231a5dc4f51ed771bf9cde1f14463d360219a08
Jerell James <jerell1998@gmail.com>
2023-08-16 12:23:26 +0100

    tie

--- src/components/plot/three/balloon/BalloonScene.tsx
+++ src/components/plot/three/balloon/BalloonScene.tsx
@@ -3,6 +3,7 @@
 import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 import { CirclingLight } from './CirclingLight';
+import { Vector3 } from 'three';
 
 export default function BalloonScene() {
   return (
@@ -10,9 +11,9 @@ export default function BalloonScene() {
       <ambientLight />
       <CirclingLight position={[0, 5, 0]} radius={5} offsetZ={10} />
 
-      <Balloon position={[10, 1, 5]} />
-      <Balloon position={[-2, 2, 2]} />
-      <Balloon position={[2, 2, 2]} />
+      <Balloon position={[-4, 2, 2]} scale={1.5} />
+      <Balloon position={[0, 2, 2]} scale={1.5} />
+      <Balloon position={[4, 2, 2]} scale={1.5} />
     </Canvas>
   );
 }
@@ -24,20 +25,35 @@ function Balloon(props: ThreeElements['mesh']) {
 
   useFrame((state, delta) => (mesh.current.rotation.y += delta));
 
+  const position = props.position as [number, number, number];
+  const tiePosition = [
+    position[0],
+    position[1] - (props.scale as number),
+    position[2],
+  ];
+
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <sphereGeometry args={[1, 32, 16]} />
-      <meshStandardMaterial
-        visible
-        color={hovered || active ? '#00fff5' : '#7dce82'}
-      />
-    </mesh>
+    <>
+      <mesh
+        {...props}
+        ref={mesh}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <sphereGeometry args={[1, 32, 16]} />
+        <meshStandardMaterial
+          visible
+          color={hovered || active ? '#00fff5' : '#7dce82'}
+        />
+      </mesh>
+      <mesh position={new Vector3(...tiePosition)}>
+        <coneGeometry args={[0.3, 0.8, 5]} />
+        <meshStandardMaterial
+          visible
+          color={hovered || active ? '#00fff5' : '#7dce82'}
+        />
+      </mesh>
+    </>
   );
 }
