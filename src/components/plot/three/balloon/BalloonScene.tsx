a82a12c0bbfeb59f8f8133a3f6bb524720bb2ca1
Jerell James <jerell1998@gmail.com>
2023-08-16 12:39:56 +0100

    number of balloons

--- src/components/plot/three/balloon/BalloonScene.tsx
+++ src/components/plot/three/balloon/BalloonScene.tsx
@@ -1,20 +1,44 @@
 'use client';
 
 import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import { CirclingLight } from './CirclingLight';
 import { Vector3 } from 'three';
+import Button from '@/components/buttons/Button';
 
 export default function BalloonScene() {
-  return (
-    <Canvas camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 20] }}>
-      <ambientLight />
-      <CirclingLight position={[0, 5, 0]} radius={5} offsetZ={10} />
+  const [numBalloons, setNumBallons] = useState<number>(1);
+  const addBalloon = () => setNumBallons((prev) => prev + 1);
+  const removeBalloon = () => setNumBallons((prev) => Math.max(0, prev - 1));
 
-      <Balloon position={[-4, 2, 2]} scale={1.5} />
-      <Balloon position={[0, 2, 2]} scale={1.5} />
-      <Balloon position={[4, 2, 2]} scale={1.5} />
-    </Canvas>
+  const scale = 3;
+  const spacingX = scale * 2.1;
+
+  const getBalloonPosX = (i: number) =>
+    spacingX * (i - Math.floor(numBalloons / 2));
+
+  const balloons = Array.from({ length: numBalloons }, (_, i) => (
+    <Balloon position={[getBalloonPosX(i), 2, 2]} scale={scale} key={i} />
+  ));
+
+  return (
+    <Suspense>
+      <div className='grid grid-cols-2 gap-2 text-center'>
+        <Button className='w-full justify-center' onClick={addBalloon}>
+          +
+        </Button>
+        <Button className='w-full justify-center' onClick={removeBalloon}>
+          -
+        </Button>
+      </div>
+      <Canvas camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 30] }}>
+        <ambientLight />
+        <CirclingLight position={[0, 5, 0]} radius={5} offsetZ={10} />
+
+        {balloons}
+      </Canvas>
+      <p>You have selected {numBalloons} balloons.</p>
+    </Suspense>
   );
 }
 
@@ -48,7 +72,13 @@ function Balloon(props: ThreeElements['mesh']) {
         />
       </mesh>
       <mesh position={new Vector3(...tiePosition)}>
-        <coneGeometry args={[0.3, 0.8, 5]} />
+        <coneGeometry
+          args={[
+            (props.scale as number) * 0.2,
+            (props.scale as number) * 0.5,
+            5,
+          ]}
+        />
         <meshStandardMaterial
           visible
           color={hovered || active ? '#00fff5' : '#7dce82'}
